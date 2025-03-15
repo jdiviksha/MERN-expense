a25081a2408e92c376efd5010139a64c57fb56af
jdiviksha <jdivikshamakthalinb@gmail.com>
2025-03-15 22:35:06 +0530

    login

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+
+} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return (
-    <div className='text-3xl text-purple-600'>Hi</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App
+const Root = () => {
+  //check token exist in local storage
+  const isAuthenticated = !!localStorage.getItem("token");
+  //redirect dashboard is authenticated or go to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"/>
+  ) : (
+    <Navigate to="/login" />
+  );
+};
---
+++ frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Expense = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default Expense
